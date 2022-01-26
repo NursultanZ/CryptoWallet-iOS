47c92a95d592d39bfb778cb06cf2eeb0917797d6
Nursultan Zakirov <nursultanzakirov10@gmail.com>
2022-01-26 22:13:56 +0800

    Initial Commit

---
+++ FYP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FYP
+//
+//  Created by user on 26/1/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FYP/FYPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FYPApp.swift
+//  FYP
+//
+//  Created by user on 26/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct FYPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
